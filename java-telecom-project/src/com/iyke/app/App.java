fac4cea906e7042f07cfe10fb4c2f9dd09d4f7c2
Cletus Ori <cletusori@Cletuss-MacBook-Air.local>
2023-10-03 15:40:46 -0300

    update

--- java-telecom-project/src/com/iyke/app/App.java
+++ java-telecom-project/src/com/iyke/app/App.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 import com.iyke.app.beans.Customer;
 import com.iyke.app.beans.CustomerSim;
 import com.iyke.app.database.Database;
+import com.iyke.app.util.Gender;
 import com.iyke.app.util.Validations;
 
 public class App {
@@ -66,6 +67,64 @@ public class App {
         }
     
     }
+
+    private Customer registerCustomer() {
+        Scanner scan = new Scanner(System.in);
+        String customerCode, name, address, phone, gender, email;
+        Gender genderEnum;
+    
+        while (true) {
+    
+          System.out.println("\nCustomer registration terminal.\nPlease provide your customer registration information");
+    
+          System.out.print("Enter name: ");
+          name = scan.nextLine();
+          if (!Validations.validateField("name", name)) {
+            System.out.println("Invalid input. Please try again.");
+            continue;
+          }
+    
+          System.out.print("Enter address: ");
+          address = scan.nextLine();
+          if (!Validations.validateField("address", address)) {
+            System.out.println("Invalid input. Please try again.");
+            continue;
+          }
+    
+          System.out.print("Enter phone: ");
+          phone = scan.nextLine();
+          if (!Validations.validateField("phone", phone)) {
+            System.out.println("Invalid input. Please try again.");
+            continue;
+          }
+    
+          System.out.print("Enter gender: ");
+          gender = scan.nextLine();
+          if (!Validations.validateField("gender", gender)) {
+            System.out.println("Invalid input. Please try again.");
+            continue;
+          }
+          if(gender.trim().equalsIgnoreCase("m")){
+            genderEnum = Gender.MALE;
+          }else if(gender.trim().equalsIgnoreCase("f")){
+            genderEnum = Gender.FEMALE;
+          }
+    
+          System.out.print("Enter email: ");
+          email = scan.nextLine();
+          if (!Validations.validateField("email", email)) {
+            System.out.println("Invalid input. Please try again.");
+            continue;
+          }
+    
+          // Generate the customer code in the following format
+          customerCode = Customer.generateCustomerCode();
+    
+          if (!name.isEmpty() && !address.isEmpty() && !phone.isEmpty() && !gender.isEmpty() && !email.isEmpty()) {
+            return new Customer(customerCode, name, address, phone, genderEnum, email);
+          }
+        }
+      }
 
     public static void main(String[] args) {
         App app = new App();
--- java-telecom-project/src/com/iyke/app/util/Validations.java
+++ java-telecom-project/src/com/iyke/app/util/Validations.java
@@ -4,9 +4,9 @@ package com.iyke.app.util;
 public class Validations {
    
     public static boolean validateSimNumberEntry(int simNumber){
-        // if(simNumber <= 0 ){
-        //     return false;
-        // }
+        if(simNumber <= 0 ){
+            return false;
+        }
         String phoneNumber = Integer.toString(simNumber);
 
         phoneNumber = phoneNumber.trim();
@@ -20,6 +20,44 @@ public class Validations {
 
         return true;
     }
+
+    public static boolean validateField(String field, String value) {
+        if (value.trim().isEmpty() || value == null) {
+          return false;
+        } 
+        // trim user input
+        value = value.trim();
+  
+        if (field.equals("phone")) {
+          if (value.length() < 10 || value.length() > 10) {
+            return false;
+          }
+        } else if (field.equals("gender")) {
+          if (value.length() > 1 || (!value.equalsIgnoreCase("M") && !value.equalsIgnoreCase("F"))) {
+            return false;
+          }
+        } else if (field.equals("email")) {
+          // example@gmail.com
+          String[] emailParts = value.split("@");
+          if (emailParts.length == 2) {
+            //System.out.println("Splitted!");
+            if (emailParts[0].length() == 0 || emailParts[1].length() == 0) {
+              //System.out.println("Bad!");
+              // E.g. @gmail.com
+              return false;
+            }
+  
+            if (!emailParts[1].contains(".")) {
+              return false;
+            }
+  
+          } else {
+            return false;
+          }
+        }
+  
+        return true;
+      }
 
     
     
