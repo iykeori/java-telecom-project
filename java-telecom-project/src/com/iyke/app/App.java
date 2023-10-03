6954d097b7b3e4556ff7bb695e1536dcdc3dd284
Cletus Ori <cletusori@Cletuss-MacBook-Air.local>
2023-10-03 16:02:26 -0300

    updated file

--- java-telecom-project/src/com/iyke/app/App.java
+++ java-telecom-project/src/com/iyke/app/App.java
@@ -45,6 +45,7 @@ public class App {
                                 if (simDetails != null){
                                     System.out.println("Your Valid Virtual Number is  "+ simDetails.getSim().getSimNumber().toString());
                                     //prompt customer to re-enter the correct number
+                                    continue; 
                                 }else{
                                     // if customer does not have a virtual number, let him purchase VN
                                 }
